a87b1575f07590e7dd0f7b12c8d8d9d1c7c2d13c
songhui1988 <huisong.ele@gmail.com>
2014-11-20 15:41:53 -0500

    Update flatten-array.js
    Using .concat and .apply to merge nested arrays into a single array.

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -3,6 +3,16 @@ describe('flatten array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
+        flattenSortArray = function (arr) {
+            return flatten(arr).sort();
+        },
+        flattenArray = function (arr) {
+          arr = [].concat.apply([],arr);
+          return arr.some(Array.isArray) ? flattenArray(arr) : arr;
+        };
+        
+        arr = flattenSortArray(arr);
+        
     expect(arr).toEqual(expected);
   });
 });
